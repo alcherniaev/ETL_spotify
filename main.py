7564dc55e6325920d34c302cc45da6738e79fccd
alcherniaev <playtowin19@gmail.com>
2021-02-03 21:25:29 +0300

    Transform part of ETL done, except of date checking

--- main.py
+++ main.py
@@ -9,12 +9,48 @@ import _sqlite3
 
 
 
-# Extract part
+
 DATABASE_LOCATION = 'sqlite://Users/alcherniaev/PycharmProjects/ETL_spotify/my_tracks.sqlite'
 USER_ID = '05q92yrt0926gkmfrvves51wi'
-TOKEN = 'BQAkW61Tv3QPVx6p-h7jqG6wPO11Py8NSOYZUCeIq7z62m7j85hVWD-IhEf4HFM7RwoWOiK2M9l97VbcIK3pl4qGIqRAmecHqsIFPv6oVAVT3kuVgjUGMEwLN5ayzHkosfANIOsz3LXCIdg156CKq0JA5UzXjS9jiZDTidj_'
+TOKEN = "BQAPZi8oW7WSdDJ0_jC6Qzf0Ih3OJIs4tcjJy3HUAUF_9e3eoX0GjcHMagi1nX5Bux4-cQkH9LwUGzqEkJ9VRzZKeC9EvjjIXmObYDy2dwOK1eCOJR4vu--BpH1voBJ-5zlwHyvypudR14KraHy4MHm3KGtHvGnH5tuMqBtT"
+
+def check_data(df: pd.DataFrame) -> bool:
+    list_error = []
+    #  is empty ?
+    message_empty = "Nothing downloaded. Perhaps you listened no one song that day. Execution Finished"
+    assert not df.empty, list_error.append(message_empty)
+
+    # is duplicates ?
+    message_keys = "Primary Key is violated, hence there are duplicates in data"
+    assert pd.Series(df["played_at"]).is_unique, list_error.append(message_keys)
+
+    # is null values ?
+    message_null = "Null values found"
+    assert not df.isnull().values.any(), list_error.append(message_null)
+
+    '''    # is it last 24 hours ?
+    yesterday_ = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday_ = yesterday_.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].to_list()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday_:
+            print(yesterday_)
+            list_error.append("Something wrong with timestamps")
+            raise Exception("Something wrong with timestamps")
+    '''
+    if not list_error:
+        return True
+    else:
+        for error in list_error:
+            print(error)
+        return False
+
 
 if __name__ == '__main__':
+
+    # Extract part
+
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
@@ -50,5 +86,10 @@ if __name__ == '__main__':
 
     }
     song_df = pd.DataFrame(song_dict, columns=["song", "artist", "played_at", "timestamp"])
+
+
+    # Validate
+    if check_data(song_df):
+        print("Data valid, proceed to Load stage")
 
     print(song_df)
